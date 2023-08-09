8a5617433cee046950672d34f4d43b33cb98b51c
Tauri <tauri.saar@voco.ee>
2023-08-09 22:52:04 +0300

    API päringud mudeliga

--- models/toode.ts
+++ models/toode.ts
@@ -1,13 +1,8 @@
 export class Toode {
-    id: number;
-    name: string;
-    price: number;
-    isFood: boolean;
-
-    constructor(id: number, name: string, price: number, isFood: boolean) {
-        this.id = id;
-        this.name = name;
-        this.price = price;
-        this.isFood = isFood;
-    }
+    constructor(
+        public id: number,
+        public name: string,
+        public price: number,
+        public isActive: boolean
+    ) {}
 }
